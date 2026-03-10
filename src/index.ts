1565a81707300e96f996f15a9a395e554d2facdc
Jon W <jonwright13@gmail.com>
2026-03-10 15:54:30 +0000

    added writeToCSV function and error handling if countries array after fetching is empty

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { fetchData } from "./api.ts";
+import { writeToCSV } from "./csv-output.ts";
 import { writeToHTML } from "./html-output.ts";
 import { processCountryData } from "./processing.ts";
 
@@ -6,10 +7,18 @@ const main = async (): Promise<void> => {
   // Fetch data from the REST Countries API endpoint
   const countries = await fetchData();
 
+  if (!countries.length) {
+    console.error("Countries array is empty");
+    process.exit(1);
+  }
+
   //   Process the country data
   const processedCountries = processCountryData(countries);
 
+  //   Generate HTML and write to output file
   await writeToHTML(processedCountries);
+
+  await writeToCSV(processedCountries);
 
   console.log(processedCountries);
 };
